3d9251d36844b8c566d3ce3f2536ffc23cfb5b24
Mohamed Emad <mohamed.emad45621@gmail.com>
2024-07-22 00:59:32 +0300

    create list view

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/widgets.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
+import 'package:notes_app/widgets/notes_item.dart';
+import 'package:notes_app/widgets/notes_view_list.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -13,69 +15,10 @@ class NotesViewBody extends StatelessWidget {
           height: 60,
         ),
         CustomAppBar(),
-        NoteItem(),
+        Expanded(child: NotesListView()),
       ],
     );
   }
 }
 
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                "Flutter Tips",
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 12),
-                child: Text(
-                  "Build Your Career with Flutter",
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                ),
-              ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 30,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                "21 May 2023",
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),
-                  fontSize: 12,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
