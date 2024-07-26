738f90d5118f320b9867c1571b064b8d3a20eeee
Mohamed Emad <mohamed.emad45621@gmail.com>
2024-07-27 00:48:54 +0300

    validate input

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key, this.onTap});
 
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8), color: kPrimaryColor),
-      width: MediaQuery.of(context).size.width,
-      child: const Center(
-        child: Text(
-          "Add",
-          style: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8), color: kPrimaryColor),
+        width: MediaQuery.of(context).size.width,
+        child: const Center(
+          child: Text(
+            "Add",
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
         ),
       ),
     );
--- lib/widgets/custom_modal_sheet.dart
+++ lib/widgets/custom_modal_sheet.dart
@@ -4,40 +4,80 @@ import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/cutom_text_field.dart';
 
 class CustomModalSheet extends StatelessWidget {
-  const CustomModalSheet({super.key,this.isEdit = false});
+  const CustomModalSheet({super.key, this.isEdit = false});
 
   final bool isEdit;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 32),
+      padding: const EdgeInsets.symmetric(horizontal: 32),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 32,
-            ),
-            const CustomTextField(
-              hint: "Title",
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const CustomTextField(
-              hint: "Content",
-              maxLines: 6,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            isEdit ? Container() : const CustomButton(),
-            const SizedBox(
-              height: 30,
-            ),
-          ],
-        ),
+        child: AddNoteForm(isEdit: isEdit),
       ),
     );
   }
 }
 
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+    required this.isEdit,
+  });
+
+  final bool isEdit;
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+    String? title, subTitle;
+    return Form(
+      key: formKey,
+      autovalidateMode: autoValidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
+            hint: "Title",
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              subTitle = value;
+            },
+            hint: "Content",
+            maxLines: 6,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          widget.isEdit
+              ? Container()
+              : CustomButton(onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                  }
+                  else{
+                    autoValidateMode = AutovalidateMode.always;
+                  }
+                }),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
+    );
+  }
+}
